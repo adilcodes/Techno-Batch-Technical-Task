6efcbeacc4496754557347068b36fcf07f912afb
Adil Sheikh <ascodesofficial@gmail.com>
2024-05-10 15:14:16 +0500

    Responsiveness: View Demands Done

--- src/components/DemandTable.jsx
+++ src/components/DemandTable.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function DemandTable() {
     return (
-        <div className="d-flex">
-            <table className='table'>
+        <div className="d-flex table-responsive">
+            <table className='table table-hover'>
                 <tr className='table-head'>
                     <td className='fw-semibold py-2 px-3'>Demand Date</td>
                     <td className='fw-semibold py-2 px-3'>Station</td>
@@ -11,13 +11,15 @@ export default function DemandTable() {
                     <td className='fw-semibold py-2 px-3'>Demand Count</td>
                     <td className='fw-semibold py-2 px-3'>POC</td>
                 </tr>
-                <tr className='tr-border'>
-                    <td className='py-4 px-3'>Mark</td>
-                    <td className='py-4 px-3'>Otto</td>
-                    <td className='py-4 px-3'>Mark</td>
-                    <td className='py-4 px-3'>@mdo</td>
-                    <td className='py-4 px-3'>Otto</td>
-                </tr>
+                <tbody>
+                    <tr className='tr-border'>
+                        <td className='py-4 px-3'>Mark</td>
+                        <td className='py-4 px-3'>Otto</td>
+                        <td className='py-4 px-3'>Mark</td>
+                        <td className='py-4 px-3'>@mdo</td>
+                        <td className='py-4 px-3'>Otto</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )

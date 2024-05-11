d446fcf2cbf96bb2d967490f25210cc069788b75
Adil Sheikh <ascodesofficial@gmail.com>
2024-05-11 23:58:43 +0500

    Made the header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -58,18 +58,18 @@ export default function Header() {
   }
   return (
     <>
-      <div className="header row bg-secondary px-4 py-3">
+      <div className="header row bg-secondary px-sm-4 px-2 py-3">
         <div className="d-flex justify-content-between align-items-center">
           <h3 className="p-0 m-0 text-white">Add Demand</h3>
           <div className="btns">
             <NavLink
               to="/view-demands"
-              className="btn btn-warning rounded-3 px-4 py-2 me-3"
+              className="btn btn-warning rounded-3 px-4 py-2 me-sm-3 me-0 mb-sm-0 mb-3 d-sm-inline d-block"
             >
               View Demands
             </NavLink>
             <button
-              className="btn btn-warning rounded-3 px-4 py-2"
+              className="btn btn-warning rounded-3 px-4 py-2 d-sm-inline d-block"
               disabled={stationsList.length > 0 ? false : true}
               onClick={submitDemands}
             >

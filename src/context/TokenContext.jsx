c37587c0e7b1263cbd01911ff0df7345419859e3
Adil Sheikh <ascodesofficial@gmail.com>
2024-05-13 05:08:35 +0500

    Token Updated

--- src/context/TokenContext.jsx
+++ src/context/TokenContext.jsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from "react";
 export const TokenContextStart = createContext();
 
 export default function TokenContext(props) {
-    const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSb2xlIjoiRHJpdmVyIiwiSWQiOiI3IiwiTmFtZSI6IkFmYXEiLCJFbWFpbCI6IkFmYXEiLCJleHAiOjE3MTU1MTQ2NjQsImlzcyI6ImFtYXpvbmRzcF9hcGkiLCJhdWQiOiJhbWF6b25kc3BfYXBpIn0.8ZLFni08AI_o1ksdN5bsqDDNekq_xPMExMuvrTgROu8");
+    const [token, setToken] = useState("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJSb2xlIjoiRHJpdmVyIiwiSWQiOiI3IiwiTmFtZSI6IkFmYXEiLCJFbWFpbCI6IkFmYXEiLCJleHAiOjE3MTU2MTg3ODQsImlzcyI6ImFtYXpvbmRzcF9hcGkiLCJhdWQiOiJhbWF6b25kc3BfYXBpIn0.Ohcsh6GecRDK__PD0t9YZg-LVCJWMeBZoOOwrpWB8Z4");
 
     return (
         <TokenContextStart.Provider value={{ token }}>
